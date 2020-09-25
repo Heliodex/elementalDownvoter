09574274ddc8f10fb067f1899e2eb94945304e6e
HelioDex <50165230+HelioDex@users.noreply.github.com>
2020-09-25 23:02:20 +0100

    Need to write more code

---
+++ parseScreenshot.h
@@ -0,0 +1,21 @@
+
+// Heliodex 2020/9/25 //
+
+#pragma once
+
+
+int color3(input)
+{
+	char f = input;
+	int color3 = 0xFFFFFFFF;
+
+	return color3;
+}
+
+int main()
+{
+	
+	// write code here //
+
+	return 0;
+}
